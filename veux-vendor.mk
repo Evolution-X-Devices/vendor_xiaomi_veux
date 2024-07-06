a56f93ea1fa733e8a0822301fd10143544baa6e7
Bruno Martins <bgcngm@gmail.com>
2024-07-06 15:27:25 +0100

    veux: Remove software OMX blobs
    Change-Id: I366be8c27a30606b8840cc6c1d38ab4dde5ef595
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- veux-vendor.mk
+++ veux-vendor.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aa9d6e1b2ab424aa54f1516445e6250865563d8a90d28ab6b70ae55ce25f3b8e
-size 186748
+oid sha256:3e2f2ee1e4a67b23aba2df8f4cbd6716a52d910d0b60a4bc7ea3d36ccb954ef5
+size 185026
