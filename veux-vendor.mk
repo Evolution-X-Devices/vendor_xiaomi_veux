92208c183aca672fbfde0d1dbddc425d4061e2c0
Frost <159105703+Karan-Frost@users.noreply.github.com>
2024-07-19 13:30:51 +0000

    veux: Extract blobs from V816.0.5.0.TKCMIXM
    Signed-off-by: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- veux-vendor.mk
+++ veux-vendor.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66831576a956101e6ad94a9f41dd9786fa05ad630d7332f11e6ba4e2e3319be1
-size 186687
+oid sha256:6ccb129e25e3aa83d283638c708f63bca96eccf38e9f3986c4f5864a263f6baf
+size 187004
